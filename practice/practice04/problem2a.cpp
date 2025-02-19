2298dbf1c47b4cde68f32afb1e157e06dd81c3ac
Achilleus2 <157391144+Achilleus2@users.noreply.github.com>
2025-02-19 07:31:59 -0600

    Create problem2a.cpp

---
+++ practice/practice04/problem2a.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+
+/*template <typename T, typename U>
+auto add(T x, U y);
+
+
+int main()
+{
+
+
+	return 0;
+}
+
+template<typename T, typename U>
+auto add(T x, U y)
+{
+
+}
+*/
