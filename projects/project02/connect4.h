75d06205bb1d1a43ea650e3c1a8e8a342f0d8e96
Achilleus2 <157391144+Achilleus2@users.noreply.github.com>
2025-05-05 06:32:34 -0500

    Update connect4.h

--- projects/project02/connect4.h
+++ projects/project02/connect4.h
@@ -14,8 +14,6 @@ public:
 	enum gameState { onGoing, player1Wins, player2Wins, draw };
 	enum Connect4::gameState status() const;
 	void display() const;
-	std::vector<std::vector<int>> makeBoard();
 	int playerTurn() const;
-	int columnAvailibility(const std::vector<std::vector<int>>& nums, int columnNum);
 	friend std::ostream& operator<<(std::ostream& out, const Connect4& game);
 };
