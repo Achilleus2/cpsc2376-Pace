51cec3af108f69da64354202a0ec91b35883bd92
Achilleus2 <157391144+Achilleus2@users.noreply.github.com>
2025-05-05 06:33:35 -0500

    Create PointerPart1.cpp

---
+++ ConsoleApplication1/Tutorial9/PointerPart1.cpp
@@ -0,0 +1,18 @@
+/*
+#include <iostream>
+
+int main() {
+    int x = 10;         // Declare and initialize an integer
+    int* y = nullptr;   // Declare a pointer initialized to nullptr
+
+    // Assign the address of x to y
+    y = &x;
+
+    // Print the values
+    std::cout << "x: " << x << std::endl;
+    std::cout << "y: " << y << std::endl;
+    std::cout << "*y: " << *y << std::endl;
+    std::cout << "&x: " << &x << std::endl;
+
+    return 0;
+}*/
